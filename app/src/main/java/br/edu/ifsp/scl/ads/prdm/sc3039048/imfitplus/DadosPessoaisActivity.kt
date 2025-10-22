adff089da8623f156387e016c678c41183547228
PeHenr1 <phenriqueaissa@gmail.com>
2025-10-21 22:41:01 -0300

    feat: atualizar idade para NumberPicker e sexo para Spinner
    - foram feitos também ajustes de tela, como espaçamentos e pequenas melhorias visuais
    - foram feitos ajustes de lógica da DadosPessoaisActivity

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039048/imfitplus/DadosPessoaisActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039048/imfitplus/DadosPessoaisActivity.kt
@@ -1,9 +1,9 @@
 package br.edu.ifsp.scl.ads.prdm.sc3039048.imfitplus
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
+import android.os.Bundle
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 
 class DadosPessoaisActivity : AppCompatActivity() {
 
@@ -12,34 +12,37 @@ class DadosPessoaisActivity : AppCompatActivity() {
         setContentView(R.layout.activity_dados_pessoais)
 
         val edtNome = findViewById<EditText>(R.id.edtNome)
-        val edtIdade = findViewById<EditText>(R.id.edtIdade)
         val edtAltura = findViewById<EditText>(R.id.edtAltura)
         val edtPeso = findViewById<EditText>(R.id.edtPeso)
-        val rgSexo = findViewById<RadioGroup>(R.id.rgSexo)
-        val spinnerAtividade = findViewById<Spinner>(R.id.spinnerAtividade)
-        val btnCalcularIMC = findViewById<Button>(R.id.btnCalcularIMC)
 
+        val npIdade = findViewById<NumberPicker>(R.id.npIdade)
+        npIdade.minValue = 1
+        npIdade.maxValue = 120
+        npIdade.wrapSelectorWheel = true
+
+        val spinnerSexo = findViewById<Spinner>(R.id.spinnerSexo)
+
+        val spinnerAtividade = findViewById<Spinner>(R.id.spinnerAtividade)
         val atividades = arrayOf("Sedentário", "Leve", "Moderado", "Intenso")
         spinnerAtividade.adapter =
             ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, atividades)
 
+        val btnCalcularIMC = findViewById<Button>(R.id.btnCalcularIMC)
         btnCalcularIMC.setOnClickListener {
             val nome = edtNome.text.toString().trim()
-            val idadeStr = edtIdade.text.toString().trim()
+            val idade = npIdade.value
             val alturaStr = edtAltura.text.toString().trim()
             val pesoStr = edtPeso.text.toString().trim()
-            val sexoId = rgSexo.checkedRadioButtonId
+            val sexo = spinnerSexo.selectedItem.toString()
             val nivelAtividade = spinnerAtividade.selectedItem.toString()
 
-            if (nome.isEmpty() || idadeStr.isEmpty() || alturaStr.isEmpty() || pesoStr.isEmpty() || sexoId == -1 ) {
+            if (nome.isEmpty() || alturaStr.isEmpty() || pesoStr.isEmpty()) {
                 Toast.makeText(this, "Preencha todos os campos!", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
-            val idade = idadeStr.toInt()
             val altura = alturaStr.toFloat()
             val peso = pesoStr.toFloat()
-            val sexo = findViewById<RadioButton>(sexoId).text.toString()
 
             val imc = peso / (altura * altura)
 
