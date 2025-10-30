3b0f74995afe2c7efce2c9c4d5ce12377e5c1521
PeHenr1 <phenriqueaissa@gmail.com>
2025-10-30 00:06:15 -0300

    gardening

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039048/imfitplus/ui/GastoCaloricoActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039048/imfitplus/ui/GastoCaloricoActivity.kt
@@ -29,12 +29,10 @@ class GastoCaloricoActivity : AppCompatActivity() {
         }
 
         usuario?.let {
-            // 1. Define a mensagem personalizada
             tvMensagemGasto.text = getString(R.string.msg_gasto_personalizada, it.nome)
 
-            // 2. Calcula e exibe a TMB
             val tmb = Calculos.calcularTMB(it)
-            tvResultado.text = "%.1f kcal".format(tmb)
+            tvResultado.text = "%.2f kcal".format(tmb)
 
             btnCalcularPesoIdeal.setOnClickListener {
                 val intent = Intent(this, PesoIdealActivity::class.java)
